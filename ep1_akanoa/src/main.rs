be6079b9271114df6163b96a27567c433d08f3e3
nuadajbg <d3c07604@protonmail.com>
2024-01-16 21:16:39 +0100

    ep1 1:43:35

--- ep1_akanoa/src/main.rs
+++ ep1_akanoa/src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
-    let t :f64 = -12.1234523534252345324523452165476787;
-    println!("Hello, world! {t}");
+
+    let mut my: (&str, i32, f64, bool, (&str, i32))= ("name",42,12.54,false, ("plop",4));
+
+    my.3 = true;
+    my.4.1 = 5;
+
+    println!("Hello, world! {my:?}");
 }
