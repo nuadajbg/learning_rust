a5dabee0ba771737a188eb27e836978dbfda659b
nuadajbg <d3c07604@protonmail.com>
2024-01-18 07:48:05 +0100

    plop

--- ep1_akanoa/src/main.rs
+++ ep1_akanoa/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     let x : i32 = 42;
     let y : bool = false;
 
-    if x == 42 {
+    if y == true {
         println!("on est dans le if");
     } else if x < 52 {
         println!("plop")
