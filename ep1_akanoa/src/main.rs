243cd8048ee577c1a5105fb2c90e9cc47037a0aa
nuadajbg <d3c07604@protonmail.com>
2024-01-21 21:27:45 +0100

    ep2 54:32

--- ep1_akanoa/src/main.rs
+++ ep1_akanoa/src/main.rs
@@ -1,13 +1,32 @@
 fn main() {
 
-    let x : i32 = 42;
-    let y : bool = false;
+    let matrice : [[u8; 2]; 2] = [[2,4],[1,3]];
 
-    if y == true {
-        println!("on est dans le if");
-    } else if x < 52 {
-        println!("plop")
+  let array: [i32; 4] = [2, 4, 17, 19];
+  let size = 4;
+  let mut iteration = 0;
+
+  loop {
+    if iteration < size {
+        println!("iteration est {iteration}");
+        println!("{}",array[iteration]);
+        iteration += 1
     } else {
-        println!("plop");
+        break
     }
+  };
+
+  iteration=0;
+
+while iteration < size {
+    println!("iteration est {iteration}");
+    println!("{}",array[iteration]);
+    iteration += 1
+
+}
+
+   // println!("Le tableau vaut {array:?}");
+   // println!("{}",array[2]);
+   // println!("La matrice vaut {matrice:?}");
+   // println!("{}",matrice[1.1]);
 }
