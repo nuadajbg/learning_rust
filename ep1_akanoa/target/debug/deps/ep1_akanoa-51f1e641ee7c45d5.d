d1df11ec3895daa9afd2ecb4ca09d3ec4b9705c1
nuadajbg <d3c07604@protonmail.com>
2024-12-20 07:08:59 +0100

    OK

---
+++ ep1_akanoa/target/debug/deps/ep1_akanoa-51f1e641ee7c45d5.d
@@ -0,0 +1,5 @@
+/Users/jb/github/learning_rust/ep1_akanoa/target/debug/deps/libep1_akanoa-51f1e641ee7c45d5.rmeta: src/main.rs
+
+/Users/jb/github/learning_rust/ep1_akanoa/target/debug/deps/ep1_akanoa-51f1e641ee7c45d5.d: src/main.rs
+
+src/main.rs:
---
+++ ep1_akanoa/target/debug/deps/ep1_akanoa-ed95e6fdf082cc3e.d
@@ -0,0 +1,5 @@
+/Users/jb/github/learning_rust/ep1_akanoa/target/debug/deps/libep1_akanoa-ed95e6fdf082cc3e.rmeta: src/main.rs
+
+/Users/jb/github/learning_rust/ep1_akanoa/target/debug/deps/ep1_akanoa-ed95e6fdf082cc3e.d: src/main.rs
+
+src/main.rs:
